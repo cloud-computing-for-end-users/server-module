0d36d40e9a7959af0d163b07c0b129d8a30c1913
gaticks <kenneth.norholm@gmail.com>
2019-09-09 11:30:15 +0200

    CC-45 have to push for the changes to be represented in the nuget package

---
+++ src/server-module/server-module-depricated/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace server_module
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server module is starting...");
+
+            ServerModule serverModule = new ServerModule();
+            ServerModuleMessageHandler responseHandler = new ServerModuleMessageHandler(serverModule);
+
+            responseHandler.Start();
+        }
+    }
+}
