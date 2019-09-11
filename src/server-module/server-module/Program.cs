579332836add0bbcc7deb9880255687add7bdeed
gaticks <kenneth.norholm@gmail.com>
2019-09-11 10:18:13 +0200

    CC-45 initial ServerModule implementation

--- src/server-module/server-module/Program.cs
+++ src/server-module/server-module/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using custom_message_based_implementation.consts;
+using custom_message_based_implementation.encoding;
+using message_based_communication.model;
+using System;
+using System.Threading;
 
 namespace server_module
 {
@@ -6,10 +10,39 @@ namespace server_module
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Booting the server module...");
+
+            checked
+            {
+                try
+                {
+                    var serverModuleRegistrationPort = new Port() { ThePort = 5523 };
+
+                    var serverModule = new ServerModule(serverModuleRegistrationPort, new ModuleType() { TypeID = ModuleTypeConst.MODULE_TYPE_SERVER_MODULE }, new CustomEncoder());
+
+                    var server_module_connection_informtion = new ConnectionInformation()
+                    {
+                        IP = new IP() { TheIP = "127.0.0.1" },
+                        Port = new Port() { ThePort = 5522 }
+                    };
+
+                    serverModule.Setup(server_module_connection_informtion, serverModuleRegistrationPort, server_module_connection_informtion, new CustomEncoder());
+
+                    Console.WriteLine("The servermodule have started sucessfully");
+                    ////test
+                    //Thread.Sleep(10000);
+                    //serverModule.ForFunTest();
+                    //Thread.Sleep(5000);
+                    //serverModule.ForFunTest();
 
 
-
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Encountered exception while starting ServerModule: " + ex.Message);
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
--- src/server-module/server-module/ServerModule.cs
+++ src/server-module/server-module/ServerModule.cs
@@ -1,10 +1,45 @@
-﻿using System;
+﻿using message_based_communication.model;
+using message_based_communication.module;
+using custom_message_based_implementation.interfaces;
+using System;
+using custom_message_based_implementation.proxy;
 using System.Collections.Generic;
-using System.Text;
+using custom_message_based_implementation.model;
+using message_based_communication.encoding;
 
 namespace server_module
 {
-    public class ServerModule : 
+    public class ServerModule : BaseRouterModule, IServerModule
     {
+        public ServerModule(Port portForRegistrationToRouter, ModuleType moduleType, Encoding customEncoding) : base(portForRegistrationToRouter, moduleType, customEncoding)
+        {
+        }
+
+        public override string CALL_ID_PREFIX => "SERVER_MODULE_CALLING_";
+
+        protected override string MODULE_ID_PREFIXES => "SERVER_MODULE_GIVEN_ID_";
+
+        public override void HandleRequest(BaseRequest message)
+        {
+            throw new NotImplementedException("Does not handle any requests yet");
+        }
+
+
+        public void ForFunTest()
+        {
+            Console.WriteLine("Trying to get list of running applications from SO");
+            new SlaveOwnerServermoduleProxy(proxyHelper,this).GetListOfRunningApplications(HandleForFunResponse);
+        }
+
+        private void HandleForFunResponse(List<ApplicationInfo> list)
+        {
+            Console.WriteLine("Applications:");
+
+            foreach (var item in list)
+            {
+                Console.WriteLine("Application name: " + item.ApplicationName);
+            }
+        }
+
     }
 }
